870c6796dfcda0ecc3aad2fa8e733c80d95ea6d8
Adhrit Das <adhrit.das.p@gmail.com>
2021-01-22 16:25:28 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function showMessage(){
+    document.getElementById('hiddenMessage').style.display='block';   
+}
+
+function countDown(){
+    var currentVal = document.getElementById('counter').innerHTML;
+    var newVal = 0;
+
+    if(currentVal>0){
+        newVal = currentVal-1;
+    }
+
+    document.getElementById('counter').innerHTML=newVal;
+}
